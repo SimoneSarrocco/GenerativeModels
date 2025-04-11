3c47a351c291aa362678ff1d7990bc26de873f91
simone.sarrocco <simone.sarrocco@studio.unibo.it>
2025-04-11 15:33:33 +0000

    all changes to train the vqgan as a denoising (image-to-image translation) model similar to pix2pix

--- tutorials/generative/2d_vqgan/dataset.py
+++ tutorials/generative/2d_vqgan/dataset.py
@@ -10,10 +10,41 @@ from PIL import Image
 import ants
 
 
+def normalize_image(image):
+    min_val = np.min(image)
+    max_val = np.max(image)
+    return (image - min_val) / (max_val - min_val)
+
+
+def crop_or_pad_image(image, target_shape):
+    """
+    Crop or pad the image to the target shape.
+    """
+    current_shape = image.shape
+    cropped_padded_image = np.zeros(target_shape)
+
+    # Calculate cropping/padding indices
+    crop_start = [(current_dim - target_dim) // 2 if current_dim > target_dim else 0 for current_dim, target_dim in
+                  zip(current_shape, target_shape)]
+    crop_end = [crop_start[i] + target_shape[i] for i in range(len(target_shape))]
+
+    pad_start = [(target_dim - current_dim) // 2 if current_dim < target_dim else 0 for current_dim, target_dim in
+                 zip(current_shape, target_shape)]
+    pad_end = [pad_start[i] + current_shape[i] for i in range(len(current_shape))]
+
+    # Crop the image
+    cropped_image = image[crop_start[0]:crop_end[0], crop_start[1]:crop_end[1]]
+
+    # Pad the image
+    cropped_padded_image[pad_start[0]:pad_end[0], pad_start[1]:pad_end[1]] = cropped_image
+
+    return cropped_padded_image
+
+
 class OCTDataset(Dataset):
     def __init__(self, images, resize=False, pixel_range=0, gaussian_noise=False, clip=False, blur=False,
                  transform=False):
-        self.images = images
+        self.image_pairs = images
         self.pixel_range = pixel_range
         self.resize = resize
         self.gaussian_noise = gaussian_noise
@@ -22,65 +53,44 @@ class OCTDataset(Dataset):
         self.transform = transform
 
     def __getitem__(self, index):
-        image = self.images[index, 0, ...]  # int, range [0, 255], and shape [H, W]
+        low_snr = self.image_pairs[index, 0, ...]  # int, range [0, 255], and shape [H, W]
+        high_snr = self.image_pairs[index, 1, ...]  # int, range [0, 255], and shape [H, W]
 
-        image = np.asarray(image, dtype=np.float32)
-        # high_snr = np.asarray(high_snr, dtype=np.float32)
-
-        # clahe = cv2.createCLAHE(clipLimit=1.5, tileGridSize=(15, 15))
-        # low_snr = clahe.apply(low_snr)
-        # Transform the ndarrays to float32 tensors of shape [1, H, W]
-        # low_snr = torch.tensor(low_snr, dtype=torch.float32).unsqueeze(0)
-        # high_snr = torch.tensor(high_snr, dtype=torch.float32).unsqueeze(0)
+        low_snr = np.asarray(low_snr, dtype=np.float32)
+        high_snr = np.asarray(high_snr, dtype=np.float32)
 
         if self.clip:
             # Clipping
-            image = np.clip(image, np.quantile(image, 0.001), np.quantile(image, 0.999))
-            # high_snr = np.clip(high_snr, np.quantile(high_snr, 0.001), np.quantile(high_snr, 0.999))
+            low_snr = np.clip(low_snr, np.quantile(low_snr, 0.001), np.quantile(low_snr, 0.999))
+            high_snr = np.clip(high_snr, np.quantile(high_snr, 0.001), np.quantile(high_snr, 0.999))
 
         # Gaussian noise
         if self.gaussian_noise:
-            # low_snr = cv2.GaussianBlur(low_snr, (3, 3), 1)
-            image = (image - np.mean(image)) / np.std(image)
-            # high_snr = (high_snr - np.mean(high_snr)) / np.std(high_snr)
-            gaussian_noise = np.random.normal(0, 0.1, image.shape)
-            image = image + gaussian_noise
-            # low_snr = np.clip(low_snr, 0, 1)
-            # low_snr = cv2.addWeighted(low_snr, 1.5, gaussianblurred, -0.5, 0)
-            # clahe = cv2.createCLAHE(clipLimit=127.5, tileGridSize=(8, 8))
-            # low_snr = clahe.apply(low_snr.astype(np.uint8))
+            low_snr = (low_snr - np.mean(low_snr)) / np.std(low_snr)
+            gaussian_noise = np.random.normal(0, 0.1, low_snr.shape)
+            low_snr = low_snr + gaussian_noise
 
         if self.blur:
-            image = cv2.GaussianBlur(image, (5, 5), 1)
+            low_snr = cv2.GaussianBlur(low_snr, (5, 5), 1)
 
         # Transform the ndarrays to float32 tensors of shape [1, H, W]
-        image = torch.tensor(image, dtype=torch.float32).unsqueeze(0)
-        # high_snr = torch.tensor(high_snr, dtype=torch.float32).unsqueeze(0)
+        low_snr = torch.tensor(low_snr, dtype=torch.float32).unsqueeze(0)
+        high_snr = torch.tensor(high_snr, dtype=torch.float32).unsqueeze(0)
 
         if self.resize:
             resize = transforms.Resize((248, 384))
-            image = resize(image)
-            # high_snr = resize(image)
-            if self.pixel_range == 0:
-                padding = transforms.Pad((0, 4, 0, 4), fill=0)
-            elif self.pixel_range == -1:
-                padding = transforms.Pad((0, 4, 0, 4), fill=-1)
+            low_snr = resize(low_snr)
+            high_snr = resize(high_snr)
+            padding = transforms.Pad((0, 4, 0, 4), fill=0)
         else:
-            if self.pixel_range == 0:
-                padding = transforms.Pad((0, 8, 0, 8), fill=0)
-            elif self.pixel_range == -1:
-                padding = transforms.Pad((0, 8, 0, 8), fill=-1)
-
-        # Standardize them
-        # low_snr = (low_snr - torch.mean(low_snr)) / torch.std(low_snr)
-        # high_snr = (high_snr - torch.mean(high_snr)) / torch.std(high_snr)
+            padding = transforms.Pad((0, 8, 0, 8), fill=0)
 
         # Normalize to [0,1] each image separately by min-max
-        image = (image - torch.min(image)) / (
-                torch.max(image) - torch.min(image))
+        low_snr = (low_snr - torch.min(low_snr)) / (
+                torch.max(low_snr) - torch.min(low_snr))
 
-        # high_snr = (high_snr - torch.min(high_snr)) / (
-        #        torch.max(high_snr) - torch.min(high_snr))
+        high_snr = (high_snr - torch.min(high_snr)) / (
+                torch.max(high_snr) - torch.min(high_snr))
 
         if self.transform:
             trans = transforms.Compose([
@@ -88,35 +98,21 @@ class OCTDataset(Dataset):
                 # transforms.RandomRotation(degrees=5),
                 # transforms.ColorJitter(0.2, 0.2),
             ])
-            image = trans(image)
-            # low_snr = transformed_images[0]
-            # high_snr = transformed_images[1]
-
-        """
-        # Standardize them
-        low_snr = (low_snr - torch.mean(low_snr)) / torch.std(low_snr)
-        high_snr = (high_snr - torch.mean(high_snr)) / torch.std(high_snr)
-
-        # Normalize them to [0,1]
-        preprocessed_low_snr = (preprocessed_low_snr - torch.min(preprocessed_low_snr)) / (
-                torch.max(preprocessed_low_snr) - torch.min(preprocessed_low_snr))
-
-        preprocessed_high_snr = (preprocessed_high_snr - torch.min(preprocessed_high_snr)) / (
-                torch.max(preprocessed_high_snr) - torch.min(preprocessed_high_snr))
-        """
-        if self.pixel_range == -1:
-            # Linearly scale each image from [0,1] to [-1,1]
-            # preprocessed_low_snr = 2 * preprocessed_low_snr - 1
-            # preprocessed_high_snr = 2 * preprocessed_high_snr - 1
-            normalization = transforms.Normalize(0.5, 0.5)
-            image = normalization(image)
-            # high_snr = normalization(high_snr)
+            concatenation = torch.stack((low_snr, high_snr), dim=0)
+            transformed_images = trans(concatenation)
+            low_snr = transformed_images[0]
+            high_snr = transformed_images[1]
 
         # Pad each image to either [256, 384] or [496, 768] depending on whether we have resized the images or not
-        image = padding(image)
-        # high_snr = padding(high_snr)
+        low_snr = padding(low_snr)
+        high_snr = padding(high_snr)
 
-        return image
+        if self.pixel_range == -1:
+            # Linearly scale each image from [0,1] to [-1,1]
+            low_snr = 2 * low_snr - 1
+            high_snr = 2 * high_snr - 1
+
+        return low_snr, high_snr
 
     def __len__(self):
-        return len(self.images)
+        return len(self.image_pairs)
